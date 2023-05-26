fec3e746c17ef5ea9d11d425899e20c388d7c0cd
22013446 <22013446@10.174.112.71>
2023-05-26 14:30:19 +0800

    Ashley

---
+++ P00/test/CalculatorTest.java
@@ -0,0 +1,71 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAdd() {
+		//fail("Not yet implemented");
+		int a = 1234;
+		int b = 8765;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.add(a, b);
+		
+		int expected = 9999;
+		assertEquals(expected,actual);
+	}
+	public void testSubtract() {
+		int a = 4567;
+		int b = 3210;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.subtract(a, b);
+		
+		int expected = 1357;
+		assertEquals(expected,actual);
+	}
+	public void multiple() {
+		int a = 123;
+		int b = 2;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.multiple(a, b);
+		
+		int expected = 246;
+		assertEquals(expected,actual);
+	}
+	public void devide() {
+		int a = 10;
+		int b = 5;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.devide(a, b);
+		
+		int expected = 2;
+		assertEquals(expected,actual);
+		
+	}
+
+}
